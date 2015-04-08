115f62e1a2944cb727f0cf0960d8f06814a09c71
Irina Semenova <ii_osoba@mail.ru>
2015-04-08 10:31:01 +0300

    Create CalcMain.java

---
+++ CalcMain.java
@@ -0,0 +1,26 @@
+import java.awt.Container;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import calculator.CalcPanel;
+
+
+public class CalcMain {
+
+	public static void main(String[] args) {
+		JFrame theGUI = new JFrame();
+		theGUI.setTitle("My Calculator");
+		
+		//инзменила ширину окна
+		theGUI.setSize(250,350);
+		theGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container pane = theGUI.getContentPane();
+		JPanel myPanel = new CalcPanel();
+		pane.add(myPanel);
+		
+		theGUI.setVisible(true);
+
+	}
+
+}
